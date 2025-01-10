3a22e30ecebd8460a5d11528d114f7452f3a4c0b
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-01-11 01:39:21 +0300

    feat: create empty window on activate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::{glib, prelude::*, Application, ApplicationWindow};
+
+const APP_ID: &str = "com.fekoneko.ppv.app";
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder().application_id(APP_ID).build();
+    app.connect_activate(build_ui);
+
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("Pixiv Powerful Viewer")
+        .build();
+
+    window.present();
 }
