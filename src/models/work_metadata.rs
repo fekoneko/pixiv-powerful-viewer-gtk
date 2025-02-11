d4c272e8301d447ba94a61a3752ae739ad33cec7
fekoneko <55813967+fekoneko@users.noreply.github.com>
2025-02-11 21:08:52 +0300

    feat: allow `WorkMetadata` to be deserialized

--- src/models/work_metadata.rs
+++ src/models/work_metadata.rs
@@ -1,9 +1,9 @@
 use chrono::prelude::*;
-use serde::{Serialize, Serializer};
+use serde::{Deserialize, Serialize, Serializer};
 
 use crate::utils::serde::*;
 
-#[derive(Serialize)]
+#[derive(Serialize, Deserialize)]
 pub struct WorkMetadata {
     pub id: usize,
     pub title: String,
@@ -13,8 +13,10 @@ pub struct WorkMetadata {
     pub user_name: String,
     pub page_count: usize,
     #[serde(serialize_with = "serialize_date_time")]
+    #[serde(deserialize_with = "deserialize_date_time")]
     pub upload_time: DateTime<Utc>,
     #[serde(serialize_with = "serialize_date_time_option")]
+    #[serde(deserialize_with = "deserialize_date_time_option")]
     pub download_time: Option<DateTime<Utc>>,
     pub is_ai: Option<bool>,
     pub bookmark_count: usize,
@@ -40,6 +42,20 @@ impl Serialize for WorkMetadataKind {
     }
 }
 
+impl<'de> Deserialize<'de> for WorkMetadataKind {
+    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        let kind = String::deserialize(deserializer)?;
+        match kind.as_str() {
+            "illustration" => Ok(WorkMetadataKind::Illustration),
+            "manga" => Ok(WorkMetadataKind::Manga),
+            _ => Err(serde::de::Error::custom(format!(
+                "invalid work kind: {}",
+                kind
+            ))),
+        }
+    }
+}
+
 pub enum WorkMetadataAgeRestriction {
     AllAges,
     R18,
@@ -52,6 +68,21 @@ impl Serialize for WorkMetadataAgeRestriction {
             WorkMetadataAgeRestriction::AllAges => serializer.serialize_str("all_ages"),
             WorkMetadataAgeRestriction::R18 => serializer.serialize_str("r18"),
             WorkMetadataAgeRestriction::R18G => serializer.serialize_str("r18g"),
+        }
+    }
+}
+
+impl<'de> Deserialize<'de> for WorkMetadataAgeRestriction {
+    fn deserialize<D: serde::Deserializer<'de>>(deserializer: D) -> Result<Self, D::Error> {
+        let restriction = String::deserialize(deserializer)?;
+        match restriction.as_str() {
+            "all_ages" => Ok(WorkMetadataAgeRestriction::AllAges),
+            "r18" => Ok(WorkMetadataAgeRestriction::R18),
+            "r18g" => Ok(WorkMetadataAgeRestriction::R18G),
+            _ => Err(serde::de::Error::custom(format!(
+                "invalid work age restriction: {}",
+                restriction
+            ))),
         }
     }
 }
--- src/utils/serde.rs
+++ src/utils/serde.rs
@@ -3,21 +3,25 @@ use std::str::FromStr;
 use chrono::prelude::*;
 use serde::{Deserialize, Deserializer, Serializer};
 
-pub fn serialize_date_time<S>(date_time: &DateTime<Utc>, serializer: S) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
+pub fn serialize_date_time<S: Serializer>(
+    date_time: &DateTime<Utc>,
+    serializer: S,
+) -> Result<S::Ok, S::Error> {
     let date_time = date_time.to_string();
     serializer.serialize_str(&date_time)
 }
 
-pub fn serialize_date_time_option<S>(
+pub fn deserialize_date_time<'de, D: Deserializer<'de>>(
+    deserializer: D,
+) -> Result<DateTime<Utc>, D::Error> {
+    let date_time = String::deserialize(deserializer)?;
+    DateTime::from_str(&date_time).map_err(serde::de::Error::custom)
+}
+
+pub fn serialize_date_time_option<S: Serializer>(
     date_time: &Option<DateTime<Utc>>,
     serializer: S,
-) -> Result<S::Ok, S::Error>
-where
-    S: Serializer,
-{
+) -> Result<S::Ok, S::Error> {
     if let Some(date_time) = date_time {
         let serialized_string = date_time.to_string();
         serializer.serialize_str(&serialized_string)
@@ -26,13 +30,17 @@ where
     }
 }
 
-pub fn deserialize_date_time<'de, D: Deserializer<'de>>(
+pub fn deserialize_date_time_option<'de, D: Deserializer<'de>>(
     deserializer: D,
-) -> Result<DateTime<Utc>, D::Error>
-where
-{
-    let date_time = String::deserialize(deserializer)?;
-    DateTime::from_str(&date_time).map_err(serde::de::Error::custom)
+) -> Result<Option<DateTime<Utc>>, D::Error> {
+    let date_time = Option::<String>::deserialize(deserializer)?;
+    if let Some(date_time) = date_time {
+        DateTime::from_str(&date_time)
+            .map(Some)
+            .map_err(serde::de::Error::custom)
+    } else {
+        Ok(None)
+    }
 }
 
 pub fn deserialize_string_to_usize<'de, D: Deserializer<'de>>(
